d39be7ceaf4dad5989ad23709eb33e1825cccf8d
Andrey <elfxf@macbook.local>
2009-03-28 14:57:08 +0100

    add some info to Detail, and migrate it add User — Detail relationships

--- db/migrate/20090328134523_create_details.rb
+++ db/migrate/20090328134523_create_details.rb
@@ -1,7 +1,23 @@
 class CreateDetails < ActiveRecord::Migration
   def self.up
     create_table :details do |t|
-
+			t.integer :user_id
+			
+			# contact info
+			t.string :icq
+			t.string :skype
+			t.string :gtalk
+			t.string :jabber
+			t.string :website
+			t.string :twitter
+			
+			# person info
+			t.boolean :sex
+			t.date :birthdate
+			t.string :country
+			t.string :city
+			
+			
       t.timestamps
     end
   end
